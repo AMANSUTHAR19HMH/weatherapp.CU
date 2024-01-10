3b662104df6177b832bf33f960c6849acf466cd3
amansuthar12 <amanallbackup@gmail.com>
2024-01-10 19:12:08 +0530

    this is weather app

---
+++ app/src/main/java/com/example/weatherapp/ApiInterface.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp
+
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @GET("weather")
+    fun getweatherData(
+        @Query("q") city: String,
+        @Query("appid") appid: String,
+        @Query("unit") unit: String
+    ): Call<weatherapp>
+
+}
